4db457e486b64946b4b641401d07aa54ea0f571d
Norman Calhoun <ndc1963@gmail.com>
2019-12-02 08:32:57 -0500

    resubmission

--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,4 @@
-
+console.log("hi");
 class Card {
     constructor (rank, suit, value) {
       this.rank = rank
@@ -8,62 +8,12 @@ class Card {
   }
   
   class Deck {
-    constructor() {
-      this.cards = []
-      
-      let ranks = [
-        "2",
-        "3",
-        "4",
-        "5",
-        "6",
-        "7",
-        "8",
-        "9",
-        "10",
-        "Jack",
-        "Queen",
-        "King",
-        "Ace"
-      ]
-      
-      let suits = [
-        "Heart",
-        "Club",
-        "Diamond",
-        "Spade"
-      ]
-    
-      /** Imperative: with `for` loops */
-  //     for (let i = 0; i < ranks.length; i++) {
-  //       for (let j = 0; j < suits.length; j++) {
-  //         let rank = ranks[i]
-  //         let suit = suits[j]
-  //         let value = i
-          
-  //         this.cards.push(new Card(rank, suit, value))
-  //       }   
-  //     }
-      
-      /** Declarative: with `Array.forEach` methods */
-      ranks.forEach((rank, value) => {
-        suits.forEach(suit => this.cards.push(new Card(rank, suit, value)))
-      })
-    } // end of Deck constructor
-    
-    /** for shuffling, research how to shuffl the contnents of an Array.
-     * if you see "Fisher-Yates" or "Knuth", your getting warm!
-     */
-  }
-  
-  const deck = new Deck()
-  
-  console.log(deck.cards)
-  class Deck {
     /*Simple 52 card deck no jokers*/
   
     constructor() {
-      this.ranks = ['2','3','4','5','6','7','8','9','10','J','Q','K','A']
+     /*this.player1 = []
+     this.player2 = [] */
+        this.ranks = ['2','3','4','5','6','7','8','9','10','J','Q','K','A']
       this.suits = ['H','S','C','D']
       this.deck = []
       this.test = 'test'
@@ -71,8 +21,16 @@ class Card {
       // Creates this.deck
       for (this.suit of this.suits){
         for (this.rank of this.ranks){
-          this.deck.push([this.suit,this.rank])}}}
-  
+          this.deck.push([this.suit,this.rank,this.value])}}}
+    /*deal(){
+        this.player1.push(this.deck.slice(0, 26))
+        this.player2.push(thilayer1, player2).deck.slice(26, 53)
+    }*/
+  /*play(){
+    player1(stack1,pop[0])
+
+
+  }*/
     _shuffle(a) {
       /**
        * Shuffles array in place.
@@ -102,5 +60,50 @@ class Card {
   
   //console.log(deck.get())
   console.log(deck.shuffle())
-  console.log(deck.draw())
+  //console.log(deck.draw())
   //console.log(deck.get())
+
+  class Player {
+      constructor() {
+        this.stack = []
+
+      }
+  }
+  let player1 = new Player();
+  let player2 = new Player();
+
+const stack1 = deck.deck.slice(0, 26)
+const stack2 = deck.deck.slice(26);
+player1.stack = stack1
+player2.stack = stack2
+
+function playWar() {
+    for(let i = 0; i < player1.stack.length; i++) {
+       for(let j = 0; j < player2.stack.length; i++) {
+           
+        if(player1.stack[i].value > player2.stack[j].value) {
+            console.log('Player1 wins')
+            player1.stack.push(i,j);
+            player1.stack.shift();
+            player2.stack.shift();
+            console.log(i)
+            console.log(j)
+            console.log(player1.stack.length)
+            console.log(player2.stack.length)
+        }
+        if(player2.stack[j].value > player1.stack[i].value) {
+            console.log('Player2 wins')
+            player2.stack.push(i,j);
+            player2.stack.shift();
+            player1.stack.shift();
+            console.log(i)
+            console.log(j)
+            console.log(player2.stack.length)
+        }
+       }
+       
+         
+    }
+     
+}
+console.log(playWar())

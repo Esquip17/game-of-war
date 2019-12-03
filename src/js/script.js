fef0cdcd4f9496b65a52dad6691d3d00aa69a40f
Norman Calhoun <ndc1963@gmail.com>
2019-12-03 08:52:46 -0500

    complete version

--- src/js/script.js
+++ src/js/script.js
@@ -14,14 +14,14 @@ class Card {
      /*this.player1 = []
      this.player2 = [] */
         this.ranks = ['2','3','4','5','6','7','8','9','10','J','Q','K','A']
-      this.suits = ['H','S','C','D']
+      this.suits = ['Hearts','Spades','Clubs','Diamonds']
       this.deck = []
       this.test = 'test'
       
       // Creates this.deck
       for (this.suit of this.suits){
         for (this.rank of this.ranks){
-          this.deck.push([this.suit,this.rank,this.value])}}}
+          this.deck.push(`${this.rank} of ${this.suit}`)}}}
     /*deal(){
         this.player1.push(this.deck.slice(0, 26))
         this.player2.push(thilayer1, player2).deck.slice(26, 53)
@@ -76,12 +76,14 @@ const stack1 = deck.deck.slice(0, 26)
 const stack2 = deck.deck.slice(26);
 player1.stack = stack1
 player2.stack = stack2
-
+console.log(player2)
+// console.log(`player1.stack[0]: ${player1.stack[0]}`)
+// console.log(`player1.stack[0][0]: ${player1.stack[0][0]}`)
 function playWar() {
     for(let i = 0; i < player1.stack.length; i++) {
        for(let j = 0; j < player2.stack.length; i++) {
            
-        if(player1.stack[i].value > player2.stack[j].value) {
+        if(player1.stack[i] > player2.stack[j]) {
             console.log('Player1 wins')
             player1.stack.push(i,j);
             player1.stack.shift();
@@ -91,7 +93,7 @@ function playWar() {
             console.log(player1.stack.length)
             console.log(player2.stack.length)
         }
-        if(player2.stack[j].value > player1.stack[i].value) {
+        if(player2.stack[j] > player1.stack[i]) {
             console.log('Player2 wins')
             player2.stack.push(i,j);
             player2.stack.shift();
